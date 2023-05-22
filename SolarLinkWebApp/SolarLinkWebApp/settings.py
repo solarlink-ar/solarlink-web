7bb0b0ef3803be1639db5334465ccc00b044626a
IvanCenyko <ivanchicago70@gmail.com>
2023-05-22 19:29:05 -0300

    settings.py corrected

--- SolarLinkWebApp/SolarLinkWebApp/settings.py
+++ SolarLinkWebApp/SolarLinkWebApp/settings.py
@@ -37,13 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-<<<<<<< Updated upstream
-    'webapp',
-    'gestionDatos'
-=======
     'home',
-    'user_mngmnt',
->>>>>>> Stashed changes
+    'user_mngmnt'
 ]
 
 MIDDLEWARE = [
