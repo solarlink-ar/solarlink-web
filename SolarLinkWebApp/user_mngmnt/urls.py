ecf5cef50a739729759ce8c1dfc8e9b877cd210c
IvanCenyko <ivanchicago70@gmail.com>
2023-06-13 14:42:24 -0300

    add login, product_id and data post test

--- SolarLinkWebApp/user_mngmnt/urls.py
+++ SolarLinkWebApp/user_mngmnt/urls.py
@@ -4,5 +4,6 @@ from . import views
 urlpatterns = [
     path("", include("django.contrib.auth.urls")),
     path("register/", views.register, name="register"),
-    path("registered/", views.registered, name="registered")
+    path("product/", views.product, name="product"),
+    path("data", views.data, name="data")
 ]
--- SolarLinkWebApp/user_mngmnt/views.py
+++ SolarLinkWebApp/user_mngmnt/views.py
@@ -1,17 +1,19 @@
-from django.shortcuts import render
 from django.http import HttpResponse, HttpResponseRedirect
-from django.template import loader
 from django.contrib.auth.models import User
+from django.shortcuts import render
+from django.template import loader
+from django.contrib import auth
+from . import models
 import datetime
 
 
-def registered(request):
+def register(request):
     if request.method == "POST":
         next_url = request.GET.get('next')
         email = request.POST['email']
         username = request.POST['username']
         password = request.POST['password']
-        password_conf =request.POST['password_conf']
+        password_conf = request.POST['password_conf']
 
         if password_conf == password:
             try:
@@ -29,8 +31,64 @@ def registered(request):
         else:
             error = 'Las contraseñas no concuerdan!'
             return render(request, 'register.html', {'error': error})
+    else:
+        return render(request, 'register.html')
+
+def product(request):
+    if request.method == "POST":
+        next_url = request.GET.get('next')
+        username = request.POST['username']
+        password = request.POST['password']
+        product_id = request.POST['product_id']
+
+        user = auth.authenticate(username=request.POST['username'],password = request.POST['password'])
+        if user:
+            auth.login(request, user)
+            models.User_link(user=user, product_id = product_id).save()
+
+            return HttpResponse('nashe')
+        else:
+            error = 'El usuario no existe'
+            return render(request, 'product.html', {'error': error})
+    else:
+        return render(request, 'product.html')
+
+def login(request):
+    if request.method == "POST":
+        username = request.POST['username']
+        password = request.POST['password']
+        user = auth.authenticate(username=request.POST['username'],password = request.POST['password'])
+        
+        if user:
+            auth.login(request, user)
+            email = user.email
+            product_id = user.user_link.product_id
+
+            data = models.Datos.objects.filter(product_id = product_id)
+            print(data)
+            
+            for i in data:
+                print(i.consumo_mins, i.voltaje_mins)
+
+        
+            return HttpResponse(f'{email}, {data}')
+
+        else:
+            error = 'El usuario no existe'
+            return render(request, 'registration/login.html', {'error': error})
+    else:
+        return render(request, "login.html")
 
 
-def register(request):
-    return render(request, 'register.html')
+def data(request):
+    if request.method == "POST":
+        product_id = request.POST['product_id']
+        consumo = request.POST['consumo']
+        voltaje = request.POST['voltaje']
+        tiempo = datetime.datetime.now()
+
+        models.Datos.objects.create(product_id = product_id, consumo_mins=consumo, voltaje_mins=voltaje, tiempo = tiempo)
+        return HttpResponse("Hecho!")
+    else:
+        return render(request, "data.html")
 
