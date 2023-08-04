f438220c581e9da472fbf7d79fbffa0812be0d0a
IvanCenyko <ivanchicago70@gmail.com>
2023-08-04 14:21:40 -0300

    model update

--- SolarLinkWebApp/user_mngmnt/models.py
+++ SolarLinkWebApp/user_mngmnt/models.py
@@ -7,10 +7,12 @@ class User_link(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     product_id = models.CharField(max_length=50)
 
-class Datos(models.Model):
-    voltaje_mins = models.FloatField(default=None)
-    consumo_mins = models.FloatField(default=None)
-    tiempo = models.TimeField(default=None)
+class Datos_hora(models.Model):
+    voltaje_hora = models.FloatField(default=None)
+    consumo_hora = models.FloatField(default=None)
+    dia = models.IntegerField(default=None)
+    mes = models.IntegerField(default=None)
+    hora = models.IntegerField(default=None)
     product_id = models.CharField(max_length=50)
 
 
