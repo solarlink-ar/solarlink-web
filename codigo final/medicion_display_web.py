fc6aef18bb5be51820b2078b85176e158b848eae
IvanCenyko <ivanchicago70@gmail.com>
2023-08-07 20:21:07 -0300

    POO reorg

--- codigo final/medicion_display_web.py
+++ codigo final/medicion_display_web.py
@@ -1,21 +1,19 @@
+from solarlink import Solarlink
 import machine, time, json, math, ads1115, network
 from machine import Pin, ADC, I2C, Timer
 from lcd_api import LcdApi
 from i2c_lcd import I2cLcd
 import urequests as requests
+
 #
 #
 suma_adc_e = 0
 suma_adc_i = 0
 index = 0
 valores_sens_corriente = []
-v_max_1 = 0
-v_max_2 = 0
+sens_corriente_1 = 0
+sens_corriente_2 = 0
 timercount = False
-#
-#
-def calculo_corriente(v_max):
-    return ((8.65714286 * v_max) / math.sqrt(2)) * 1.17 #10.1288571642
 #
 #
 def values_restart():
@@ -23,14 +21,14 @@ def values_restart():
     global suma_adc_i
     global index
     global valores_sens_corriente
-    global v_max_1
-    global v_max_2
+    global sens_corriente_1
+    global sens_corriente_2
     suma_adc_e = 0
     suma_adc_i = 0
     index = 0
     valores_sens_corriente = []
-    v_max_1 = 0
-    v_max_2 = 0
+    sens_corriente_1 = 0
+    sens_corriente_2 = 0
 #
 #
 def callback_web(tim1): #minute callback
@@ -49,62 +47,66 @@ while not wlan.isconnected():
         wlan.connect('Red Alumnos', '') #network connection
     except:
         pass'''
-#
-#
-i2c = I2C(1, scl=Pin(22), sda=Pin(21), freq=400000) #i2c init
-adc_e = ads1115.ADS1115(i2c, 72, 0) #adc externo init
 
-p33 = Pin(33, Pin.IN) #pin init
-adc_i = ADC(p33) #adc interno init
-adc_i.atten(ADC.ATTN_2_5DB) #atenuacion adc interno
 #
 #
-lcd = I2cLcd(i2c, 0x27, 4, 20) #inicio lcd
+solarlink = Solarlink()
+solarlink.init()
 #
 #
 while 1:
-    v_act_sens_i_1 = adc_e.raw_to_v(adc_e.read(7,2,3))
-    if v_act_sens_i_1 > v_max_1:
-        v_max_1 = v_act_sens_i_1
+    corriente_actual_1 = solarlink.corriente_dif_read(2, 3)
+    if corriente_actual_1 > sens_corriente_1:
+        sens_corriente_1 = corriente_actual_1
         
-    v_act_sens_i_2 = adc_e.raw_to_v(adc_e.read(7,0,1))
-    if v_act_sens_i_2 > v_max_2:
-        v_max_2 = v_act_sens_i_2
+    corriente_actual_2 = solarlink.corriente_dif_read(0, 1)
+    if corriente_actual_2 > sens_corriente_2:
+        sens_corriente_2 = corriente_actual_2
     
     #
     #
-    v_act_sens_v = adc_i.read_uv() / 1000000 * 222 / 0.9580893 # mido V del sens. tension de linea rms
-    suma_adc_i += v_act_sens_v
+    voltaje_actual = solarlink.voltaje_read() # mido V del sens. tension de linea rms
+    suma_adc_i += voltaje_actual
     index += 1 # index para el promedio
     #
     #
     if timercount:
-        lcd.clear()
-        sens_corriente_1 = calculo_corriente(v_max_1)
-        sens_corriente_2 = calculo_corriente(v_max_2)
+        solarlink.lcd.clear()
         sens_voltaje = int(suma_adc_i/index)
         consumo_1 = sens_voltaje * sens_corriente_1
         consumo_2 = sens_voltaje * sens_corriente_2
         #
         #
-        lcd.move_to(5, 0)
-        lcd.putstr(f'Vin:{sens_voltaje} V')
-        lcd.move_to(0,1)
-        lcd.putstr("LINEA1")
-        lcd.move_to(0,2)
-        lcd.putstr('%.2f A' % (sens_corriente_1))
-        lcd.move_to(0,3)
-        lcd.putstr(f'{int(consumo_1)} W')
-        lcd.move_to(14, 1)
-        lcd.putstr("LINEA2")
-        lcd.move_to(14, 2)
-        lcd.putstr('%.2f A' % (sens_corriente_2))
-        lcd.move_to(14, 3)
-        lcd.putstr(f'{int(consumo_2)} W')
-        #
-        #
-        #post_data = json.dumps({'sens_voltaje': sens_voltaje, 'sens_corriente': sens_corriente, 'consumo': consumo})
-        #requests.get('https://helyivan.pythonanywhere.com/datos', data = post_data)
+
+        if sens_voltaje <= 80:
+            solarlink.lcd.move_to(0, 0)
+            solarlink.lcd.putstr('Vin: NULO O ERROR V')
+            solarlink.lcd.move_to(0,1)
+            solarlink.lcd.putstr("LINEA1")
+            solarlink.lcd.move_to(14, 1)
+            solarlink.lcd.putstr("LINEA2")
+            
+            solarlink.lcd.move_to(0,2)
+            solarlink.lcd.putstr('%.2f A' % (sens_corriente_1))
+            solarlink.lcd.move_to(14, 2)
+            solarlink.lcd.putstr('%.2f A' % (sens_corriente_2))
+            
+            solarlink.lcd.move_to(1,3)
+            solarlink.lcd.putstr('CONSUMO BAJO POR V')
+        else:
+            solarlink.lcd.move_to(5, 0)
+            solarlink.lcd.putstr(f'Vin:{sens_voltaje} V')
+
+            solarlink.lcd.move_to(0,2)
+            solarlink.lcd.putstr('%.2f A' % (sens_corriente_1))
+            solarlink.lcd.move_to(14, 2)
+            solarlink.lcd.putstr('%.2f A' % (sens_corriente_2))
+            
+            solarlink.lcd.move_to(0,3)
+            solarlink.lcd.putstr(f'{int(consumo_1)} W')
+            solarlink.lcd.move_to(14, 3)
+            solarlink.lcd.putstr(f'{int(consumo_2)} W')
+
         timercount = False
         values_restart()
     
