6b64ba370be5d97619176674b9141c8bfa242920
IvanCenyko <ivanchicago70@gmail.com>
2023-08-09 11:35:39 -0300

    celery configuration

--- SolarLinkWebApp/SolarLinkWebApp/settings.py
+++ SolarLinkWebApp/SolarLinkWebApp/settings.py
@@ -11,9 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
-import os
-from celery.schedules import crontab
 from datetime import datetime
+from celery.schedules import crontab
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
