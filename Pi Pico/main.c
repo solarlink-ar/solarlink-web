33a1625ba779cd1bb30d1818579ea05fcdf1b633
Loobo45 <maxi_testa2012@hotmail.com>
2023-08-11 01:15:16 -0700

    Update main.c
    Inicio modo ABSORTION
    Planteamiento sistema

--- Pi Pico/main.c
+++ Pi Pico/main.c
@@ -1,12 +1,86 @@
+#include <stdio.h>
+#include "pico/stdlib.h"
 #include "hardware/pwm.h"
-#include "hardware/adc.h"
-void main() {
-uint pwm = 22;
-uint slice_num = pwm_gpio_to_slice_num(22);
 
-gpio_set_function(pwm, GPIO_FUNC_PWM);
-pwm_set_wrap(slice_num, 3787);
-pwm_set_gpio_level(pwm, 1893);
-pwm_set_enabled(slice_num, true);
-while 1;
+#define PWM_WRAP                3787  // Definimos frecuencia del micro, sin prescaler, 33kHz
+#define ADC_GPIO_BATTERY        26   
+#define ADC_CHANNEL_BATTERY     0
+
+#define BATTERY_ADC_RATIO       5     // Habra un ratio de 5 a 1 en el voltaje leido por el ADC y la bat
+
+#define ABSORTION_BATTERY_VOLTAGE   13.8   // Umbral para cambio de modo
+
+#define 
+#define BATTERY_MIN_VOLTAGE     11.8       
+#define 
+
+typedef enum {
+    BULK_MODE,
+    ABSORTION_MODE,
+    FLOAT_MODE
+} charging_mode_t;
+
+// Saturador basico, se asegura que el PWM no sea ni negativo ni que se exceda del wrap
+inline static uint16_t saturador(uint16_t wrap, int16_t level) {
+    if(level > PWM_WRAP) {
+        return PWM_WRAP;
+    }
+    else if(level < 0) {
+        return 0;
+    }
+    return level;
 }
+
+int main() {
+    /* Inicializa el standard input/output de la pico */
+    stdio_init_all();
+    
+    uint8_t pwm = 22;
+    uint8_t slice = pwm_gpio_to_slice_num(pwm);
+    int16_t pwm_level = PWM_WRAP / 2;
+
+    // Se define el modo inicial como BULK
+    charging_mode_t charging_mode = BULK_MODE;
+
+    // Inicializacion del ADC
+    adc_init();
+    adc_gpio_init(ADC_GPIO_BATTERY);
+    adc_select_input(ADC_CHANNEL_BATTERY);
+
+
+    // Habilito GPIO como salida de PWM
+    gpio_set_function(pwm, GPIO_FUNC_PWM);
+    // Wrap para 33 KHz de PWM a 125 MHz de clock
+    pwm_set_wrap(slice, PWM_WRAP);
+    // 50% de ancho de pulso de partida
+    pwm_set_gpio_level(pwm, pwm_level);
+    // Habilitar PWM
+    pwm_set_enabled(slice, true);
+
+    while (true) {
+    
+        float battery_voltage = BATTERY_ADC_RATIO * adc_read() * 3.3 / (1 << 12);
+
+
+    /////////////// ABSORTION ///////////////
+
+        if(battery_voltage < ABSORTION_BATTERY_VOLTAGE) {
+            // Incremento el duty en una unidad
+            pwm_level++;
+        }
+        else {
+            // Decremento el duty en una unidad
+            pwm_level--;
+        }
+        // Verifico que no exceda los limites
+        pwm_level = saturador(PWM_WRAP, pwm_level);
+        // Ajusto PWM
+        pwm_set_gpio_level(slice, pwm_level);
+
+    ///////////////           ////////////////
+    }
+    return 0;
+}
+
+
+//fijarse voltaje ref de adc
