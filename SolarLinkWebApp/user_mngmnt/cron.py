6564425a4cd61ed946b99e5bbbd533d61444f793
IvanCenyko <ivanchicago70@gmail.com>
2023-08-15 12:37:11 -0300

    model update

--- SolarLinkWebApp/user_mngmnt/cron.py
+++ SolarLinkWebApp/user_mngmnt/cron.py
@@ -1,5 +1,13 @@
+from django.http import HttpResponse, HttpResponseRedirect, JsonResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib.auth.models import User
+from django.shortcuts import render, redirect
+from django.template import loader
+from django.contrib import auth
+from .forms import RegisterForm
+from . import models
+import datetime
 import requests
+
 def sorter():
     pass
-def test():
-    requests.get('http://127.0.0.1:8000/')
