17c5359e858157cb0f9ed3f8f03fe7d868d13c85
Mateo Inza Fior <holaloter2@gmail.com>
2023-08-28 16:44:41 -0300

    further changes to the code skeleton for the commuter

--- ESP32 micropython/conmutacion/22_6_conmutacion_por_pin/conmutacion.py
+++ ESP32 micropython/conmutacion/22_6_conmutacion_por_pin/conmutacion.py
@@ -1,14 +1,18 @@
 from machine import Pin
 import time
 
-p15 = Pin(15, Pin.IN)
+pcruce = Pin(15, Pin.IN) #Pin de entrada del cruce por cero.
+pconm = Pin(1, Pin.OUT) #1 es un placeholder porque desconozco el pin del conmutador. Pendiente arreglar lo antes posible
 
 def commuter_interrupt(pin)
     global commuterFlag 
-    commuterFlag = 1
+    commuterFlag = 1 #Al activarse la interrupción, se coloca el flag del conmutador en 1.
     
-p15.irq(trigger=3, handler=commuter_interrupt)
+pcruce.irq(trigger=3, handler=commuter_interrupt) #3 hace que la interrupción se dispare con ambos flancos.
 
 while 1:
     if commuterFlag:
-        
+        pconm = 1
+        commuterFlag = 0
+    else:
+        pconm = 0
