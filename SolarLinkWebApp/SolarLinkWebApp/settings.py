8034396f1709fc2af1f5e1723b8751ccb2c10ce6
IvanCenyko <ivanchicago70@gmail.com>
2023-09-04 20:36:04 -0300

    vercel host allowed

--- SolarLinkWebApp/SolarLinkWebApp/settings.py
+++ SolarLinkWebApp/SolarLinkWebApp/settings.py
@@ -28,7 +28,7 @@ SECRET_KEY = 'django-insecure-wo3v35!vw762hi-$z!-stv^tmf5uy)#()l2tcl@)5v@y!%w=!5
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['10.0.2.228', '127.0.0.1']
+ALLOWED_HOSTS = ['.vercel.app']
 
 # Application definition
 
