695c414216e1c756421453a59cdf509c5a407b4d
IvanCenyko <ivanchicago70@gmail.com>
2023-09-04 20:45:13 -0300

    settings path corrected
    NO CONFIAR EN TUTORIALES DE GOOGLE CHOTOS QUE NO EXPLICAN POR QUE HACEN LO QUE HACEN  Y QUE CAMBIAR PARA TU PROYECTO

--- SolarLinkWebApp/SolarLinkWebApp/wsgi.py
+++ SolarLinkWebApp/SolarLinkWebApp/wsgi.py
@@ -12,6 +12,6 @@ import os
 from django.core.wsgi import get_wsgi_application
 
 
-os.environ.setdefault("DJANGO_SETTINGS_MODULE", "django_blog.settings")
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "SolarLinkWebApp.settings")
 application = get_wsgi_application()
 app = application
