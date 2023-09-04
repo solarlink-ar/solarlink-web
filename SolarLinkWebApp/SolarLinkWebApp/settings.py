17b9162f0226448864ad34706189df48e837cd43
IvanCenyko <ivanchicago70@gmail.com>
2023-09-04 20:55:56 -0300

    whitenoise fix test

--- SolarLinkWebApp/SolarLinkWebApp/settings.py
+++ SolarLinkWebApp/SolarLinkWebApp/settings.py
@@ -52,6 +52,7 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'whitenoise.middleware.WhiteNoiseMiddleware'
 ]
 
 ROOT_URLCONF = 'SolarLinkWebApp.urls'
@@ -121,7 +122,7 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_ROOT = BASE_DIR / "production_static/"
+STATIC_ROOT = "static/"
 STATIC_URL = "static/"
 STATICFILES_DIRS = [
     BASE_DIR / "static"]
